450a2732347c79ec60d0eedd766a8331a1afd5a8
Noni-irungu <irungudiana1@gmail.com>
2024-03-12 16:20:30 -0700

    Double numbers

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''A function that prints numbers 0 -99 separated by (,) and ' ' in ascending order'''
+for y in range(0, 100):
+    if y == 99:
+        print("{}".format(y))
+    else:
+        print("{:02}".format(y), end=", ")
