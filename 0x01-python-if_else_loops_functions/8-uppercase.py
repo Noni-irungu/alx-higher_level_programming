49907bb2452ad031b96af2d5d01cd1ba0fb1bad0
Noni-irungu <irungudiana1@gmail.com>
2024-03-12 19:38:25 -0700

    uppercasing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     '''prints string in uppercase'''
     for chrt in str:
         if ord(chrt) >= 97 and ord(chrt) <= 122:
-            chrt = chr(ord(chrt) - ord('a') + ord('A'))
+            chrt = chr(ord(chrt) - 32)
         print("{}".format(chrt), end="")
     print("")
