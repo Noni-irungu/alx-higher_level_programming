5a1b3732d2095fc06584cda9f31510800a0aedd2
Noni-irungu <irungudiana1@gmail.com>
2024-03-12 19:47:07 -0700

    file 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(str):
     for chrt in str:
         if ord(chrt) >= 97 and ord(chrt) <= 122:
             chrt = chr(ord(chrt) - 32)
-        print("{}".format(chrt), end=", ")
+        print("{:s}".format(chrt), end=", ")
