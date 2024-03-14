c6367b5e482cd7587d250bee331950ddaf81fb38
Noni-irungu <irungudiana1@gmail.com>
2024-03-13 19:33:19 -0700

    Character removal

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-'''a program that creates a cpy of a string
-remving the character at the 'n' position'''
+
 def remove_char_at(str, n):
+
+    '''a program that creates a copy of a string
+    removing the character at the 'n' position'''
     if n < 0:
         return str
     else:
