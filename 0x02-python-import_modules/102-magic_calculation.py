fce0e05f546c066f3a5f4ea7fb6784f242c801b7
Noni-irungu <irungudiana1@gmail.com>
2024-03-14 14:58:16 -0700

    same as python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,15 +2,14 @@
 
 '''Functions the same as the python bytecode'''
 
+
 def magic_calculation(a, b):
 
     from magic_calculation_102 import add, sub
-
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-
     else:
-        return(sub(a, b))
+        return (sub(a, b))
