298ce8f0d446a6f37dbadc4749167ac5d398736f
Noni-irungu <irungudiana1@gmail.com>
2024-03-14 15:41:38 -0700

    hidden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,12 @@
 
 '''Prints names defined in the given module'''
 
-import hidden_4
+if __name__ == "__main__":
 
-na_mes = dir(hidden_4)
-for nam_e in na_mes:
-    if nam_e[:2] != "__":
-        print(nam_e)
+    import hidden_4
+
+    na_mes = dir(hidden_4)
+
+    for nam_e in na_mes:
+        if nam_e[:2] != "__":
+            print(nam_e)
