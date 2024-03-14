fc0112e8a6e491127a2f8e30d9e47119cf9a393f
Noni-irungu <irungudiana1@gmail.com>
2024-03-13 19:38:14 -0700

    Matching bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    '''A program that matches the provided
+    bytecode'''
+
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
