4dadac540c80a9d7f22dc425e23374eb83e6b2b6
Noni-irungu <irungudiana1@gmail.com>
2024-03-18 07:30:37 -0700

    Replacement at a specific point

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ at a specific position'''
 
 
 def new_in_list(my_list, idx, element):
-    my_list_copy = mylist.copy()
+    my_list_copy = my_list.copy()
     if idx < 0:
         return my_list_copy
     elif idx >= len(my_list):
