3b22189a2a6e2140a91ae1ba713eee5aa6012f0c
Noni-irungu <irungudiana1@gmail.com>
2024-03-18 07:02:00 -0700

    reve

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,4 +7,4 @@ in reverse'''
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         for i in range(len(my_list)):
-            print("{:d}".format(my_list[i].reverse()))
+            print("{:d}".format((my_list[i].reverse())))
