be4a18621692344eae7125a14fcb3cfe8c916959
Noni-irungu <irungudiana1@gmail.com>
2024-03-18 10:47:25 -0700

    Getting max number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,11 +5,8 @@ of a list'''
 
 
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return 'None'
-    else:
-        max_num = my_list[0]
-        for k in range(len(my_list)):
-            if my_list[i] > max_num:
-                max_num = my_list[i]
-        return max_num
+    updt_list = []
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
