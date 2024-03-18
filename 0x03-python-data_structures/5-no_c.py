dca483ac31931e6aa3f3c0a8301822dbe26fc497
Noni-irungu <irungudiana1@gmail.com>
2024-03-18 07:44:36 -0700

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''A function that removes certain
+characters'''
+
+
+def no_c(my_string):
+    updated_string = ""
+    for elements in my_string:
+        if elements == "c" and elements == "C":
+            updated_string -= elements
+        return updated_string
