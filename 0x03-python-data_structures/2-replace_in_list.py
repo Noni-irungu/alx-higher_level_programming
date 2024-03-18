018fe429c5e6a10971787dc0ee804175df2a821a
Noni-irungu <irungudiana1@gmail.com>
2024-03-18 06:34:35 -0700

    Replacement of an element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,10 +4,8 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
+    if idx > len(my_list) - 1 or idx < 0:
         return my_list
-    elif idx >= len(my_list):
-        return mylist
     else:
         my_list[idx] = element
         return my_list
