209a4607ae7caa68fa631a1851825a6bd3240e4f
Noni-irungu <irungudiana1@gmail.com>
2024-03-18 07:25:40 -0700

    Replacing an element at a specific point

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''A function that replaces a function
+at a specific position'''
+
+
+def new_in_list(my_list, idx, element):
+    my_list_copy = mylist.copy()
+    if idx < 0:
+        return my_list_copy
+    elif idx >= len(my_list):
+        return my_list_copy
+    else:
+        my_list_copy[idx] = element
+        return my_list_copy
