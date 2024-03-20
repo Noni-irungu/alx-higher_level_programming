37971abb959324a6e09dbf3a24ac02e0a4ce7ec8
Noni-irungu <irungudiana1@gmail.com>
2024-03-20 13:19:53 -0700

    Replacing values in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''A function that replaces or add
+key or value in a dictionary'''
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
