fb17c38ca493fe07a1fd3ca3bb7501a783e0f9d1
Noni-irungu <irungudiana1@gmail.com>
2024-03-20 16:21:52 -0700

    Maximum integer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,13 +5,4 @@ biggest integer value'''
 
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
-
-    rtn_ky = list(a_dictionary.keys())[0]
-    big_gest = a_dictionary[rtn_ky]
-    for m, n in a_dictionary.items():
-        if n > big_gest:
-            big_gest = n
-            rtn_ky = m
-        return (rtn_ky)
+    return max(my_dict, key=my_dict.get) if my_dict else None
