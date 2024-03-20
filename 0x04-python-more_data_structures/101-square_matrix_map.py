572b60428c27730248357ec937daea3aa725d158
Noni-irungu <irungudiana1@gmail.com>
2024-03-20 14:48:17 -0700

    Square values of a matrix

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''A function that computes the square value of
+all integers of a matrix using map'''
+
+
+def square_matrix_map(matrix=[]):
+    return list(map(lambda roe: list(map(lambda colu: colu**2, row)), matrix))
