d94665cac249d0f9348f82f1204f442b10b935d6
Noni-irungu <irungudiana1@gmail.com>
2024-03-20 16:40:32 -0700

    Matrix squared

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -5,4 +5,4 @@ all integers of a matrix using map'''
 
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda colu: colu**2, row)), matrix))
+    return list(map((lambda row: list(map((lambda j: j * j), row))), matrix))
