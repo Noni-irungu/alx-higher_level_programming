95f00286bbc406724feb7541f659315063961707
Noni-irungu <irungudiana1@gmail.com>
2024-03-20 13:23:34 -0700

    Replacing values in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,5 +5,10 @@ key or value in a dictionary'''
 
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for k in a_dictionary:
+            if k == key:
+                a_dictionary[k] = value
     return a_dictionary
