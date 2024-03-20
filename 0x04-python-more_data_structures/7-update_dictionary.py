0f081edbdb0277b2707e44896d45e32fa9c348d7
Noni-irungu <irungudiana1@gmail.com>
2024-03-20 16:06:45 -0700

    Key replacement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,10 +5,5 @@ key or value in a dictionary'''
 
 
 def update_dictionary(a_dictionary, key, value):
-    if key not in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        for k in a_dictionary:
-            if k == key:
-                a_dictionary[k] = value
+    a_dictionary[key] = value
     return a_dictionary
